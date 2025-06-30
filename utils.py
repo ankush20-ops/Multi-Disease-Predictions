87a2f371919d1523b288ea4e1e9a9c67e826a6d2
ankush20-ops <project000000qp@gmail.com>
2025-06-30 18:01:49 +0530

    Add files via upload

--- utils.py
+++ utils.py
@@ -1,19 +1,11 @@
 import os
-import requests
+import gdown
 
 def download_model():
-    url = "https://drive.google.com/uc?export=download&id=1LCBxOvygsdJSRZ9IXbTlX3DvAmd1F4Pd"
     model_path = "models/heart_disease_rf_optimized.pkl"
 
-    if not os.path.exists(model_path) or os.path.getsize(model_path) < 1000000:
-        print("📥 Downloading model from Google Drive...")
+    if not os.path.exists(model_path):
         os.makedirs("models", exist_ok=True)
-        response = requests.get(url)
-        with open(model_path, "wb") as f:
-            f.write(response.content)
-
-        # Double-check size
-        if os.path.getsize(model_path) < 1000000:
-            raise ValueError("❌ Download failed or file corrupted.")
-        else:
-            print("✅ Model downloaded successfully.")
+        url = "https://drive.google.com/uc?id=1LCBxOvygsdJSRZ9IXbTlX3DvAmd1F4Pd"
+        output = model_path
+        gdown.download(url, output, quiet=False)
